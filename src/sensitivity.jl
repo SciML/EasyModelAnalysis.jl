eb14f9d85486328dd1654aa21923a699581cdf54
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-01-26 01:22:28 +0530

    Add back _get_sensitivity

--- src/sensitivity.jl
+++ src/sensitivity.jl
@@ -1,9 +1,22 @@
+function _get_sensitivity(prob, t, x, pbounds)
+    boundvals = getfield.(pbounds, :second)
+    boundkeys = getfield.(pbounds, :first)
+    function f(p)
+        prob = remake(prob; p = Pair.(boundkeys, p))
+        sol = solve(prob, saveat = t)
+        sol(t; idxs = x)
+    end
+    return GlobalSensitivity.gsa(f, Sobol(; order = [0, 1, 2]), boundvals;
+                                 samples = 1000)
+end
+
 """
     get_sensitivity(prob, t, x, pbounds)
 
 Returns the sensitivity of the solution at time `t` and state `x` to the parameters in `pbounds`.
 """
 function get_sensitivity(prob, t, x, pbounds)
+    sensres = _get_sensitivity(prob, t, x, pbounds)
     res_dict = Dict{Symbol, Float64}()
     for i in eachindex(boundkeys)
         res_dict[Symbol(boundkeys[i], "_first_order")] = sensres.S1[i]
@@ -25,7 +38,7 @@ Creates bar plots of the first, second and total order Sobol indices that quanti
 at time `t` and state `x` to the parameters in `pbounds`.
 """
 function create_sensitivity_plot(prob, t, x, pbounds)
-    sensres = get_sensitivity(prob, t, x, pbounds)
+    sensres = _get_sensitivity(prob, t, x, pbounds)
     paramnames = String.(Symbol.(getfield.(pbounds, :first)))
     p1 = bar(paramnames, sensres.ST,
              title = "Total Order Indices", legend = false)
