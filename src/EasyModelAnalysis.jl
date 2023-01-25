ce3d10d53a82e95f22e4707fd5d3743cb0d413a6
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-01-26 00:59:23 +0530

    Add sensitivity plots

--- src/EasyModelAnalysis.jl
+++ src/EasyModelAnalysis.jl
@@ -72,12 +72,7 @@ function datafit(prob, p, t, data)
     Pair.(pkeys, res.u)
 end
 
-"""
-    get_sensitivity(prob, t, x, pbounds)
-
-Returns the sensitivity of the solution at time `t` and state `x` to the parameters in `pbounds`.
-"""
-function get_sensitivity(prob, t, x, pbounds)
+function _get_sensitivity(prob, t, x, pbounds)
     boundvals = getfield.(pbounds, :second)
     boundkeys = getfield.(pbounds, :first)
     function f(p)
@@ -85,8 +80,16 @@ function get_sensitivity(prob, t, x, pbounds)
         sol = solve(prob, saveat = t)
         sol(t; idxs = x)
     end
-    sensres = GlobalSensitivity.gsa(f, Sobol(; order = [0, 1, 2]), boundvals;
-                                    samples = 1000)
+    return GlobalSensitivity.gsa(f, Sobol(; order = [0, 1, 2]), boundvals;
+                                 samples = 1000)
+end
+
+"""
+    get_sensitivity(prob, t, x, pbounds)
+
+Returns the sensitivity of the solution at time `t` and state `x` to the parameters in `pbounds`.
+"""
+function get_sensitivity(prob, t, x, pbounds)
     res_dict = Dict{Symbol, Float64}()
     for i in eachindex(boundkeys)
         res_dict[Symbol(boundkeys[i], "_first_order")] = sensres.S1[i]
@@ -101,5 +104,28 @@ function get_sensitivity(prob, t, x, pbounds)
     return res_dict
 end
 
-export get_timeseries, get_min_t, get_max_t, datafit, get_sensitivity
+"""
+    create_sensitivity_plot(prob, t, x, pbounds)
+
+Creates bar plots of the first, second and total order Sobol indices that quantify sensitivity of the solution
+at time `t` and state `x` to the parameters in `pbounds`.
+"""
+function create_sensitivity_plot(prob, t, x, pbounds)
+    sensres = _get_sensitivity(prob, t, x, pbounds)
+    paramnames = String.(Symbol.(getfield.(pbounds, :first)))
+    p1 = bar(paramnames, sensres.ST,
+             title = "Total Order Indices", legend = false)
+    p2 = bar(paramnames, sensres.S1,
+             title = "First Order Indices", legend = false)
+    p3 = bar([paramnames[i] * "_" * paramnames[j] for i in eachindex(paramnames)
+              for j in (i + 1):length(paramnames)],
+             [sensres.S2[i, j] for i in eachindex(paramnames)
+              for j in (i + 1):length(paramnames)],
+             title = "Second Order Indices", legend = false)
+    l = @layout [a b; c]
+    plot(p2, p3, p1; layout = l, ylims = (0, 1))
+end
+
+export get_timeseries, get_min_t, get_max_t, datafit, get_sensitivity,
+       create_sensitivity_plot
 end
