5fc8bb7a55a154e67f475273ac87c23086c2796a
Yingbo Ma <mayingbo5@gmail.com>
2023-01-25 16:36:25 -0500

    Add intervention

--- src/EasyModelAnalysis.jl
+++ src/EasyModelAnalysis.jl
@@ -12,7 +12,9 @@ include("basics.jl")
 include("datafit.jl")
 include("sensitivity.jl")
 include("threshold.jl")
+include("intervention.jl")
 
 export get_timeseries, get_min_t, get_max_t, datafit, get_sensitivity,
-       create_sensitivity_plot, stop_at_threshold
+       create_sensitivity_plot, stop_at_threshold,
+       optimal_threshold_intervention
 end
