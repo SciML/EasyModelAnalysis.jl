588589d5edc1a73f5fe969e2ba8005d9bd769557
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2023-01-27 15:41:12 +0530

    Expand sensitivity docs

--- src/sensitivity.jl
+++ src/sensitivity.jl
@@ -19,7 +19,19 @@ end
 """
     get_sensitivity(prob, t, x, pbounds)
 
-Returns the sensitivity of the solution at time `t` and state `x` to the parameters in `pbounds`.
+Returns the [Sobol Indices](https://en.wikipedia.org/wiki/Variance-based_sensitivity_analysis) that quanitfy the uncertainity of the solution at time `t` and observation `x` to the parameters in `pbounds`.
+
+
+## Arguments
+  - `t`: The time of observation, the solution is stored at this time to obtain the relevant observed variable.
+  - `x`: The observation symbolic expression.
+  - `pbounds`: An array with the bounds for each parameter, passed as a pair of parameter expression and a vector with the upper and lower bound.
+
+## Keyword Arguments
+  - `samples`: Number of samples for running the global sensitivity analysis.
+
+# Returns
+  - A dictionary with the first, second and total order indices for all parameters (and pairs incase of second order).
 """
 function get_sensitivity(prob, t, x, pbounds; samples = 1000)
     sensres = _get_sensitivity(prob, t, x, pbounds; samples)
