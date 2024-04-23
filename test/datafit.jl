e1c765ed6f5375b7f2ab5ae1e7aeb7a5a18b2c08
jClugstor <jadonclugston@gmail.com>
2024-04-23 10:50:48 -0400

    increase niter in tests for baysian datafit

--- test/datafit.jl
+++ test/datafit.jl
@@ -86,7 +86,7 @@ tsave = collect(10.0:10.0:100.0)
 sol_data = solve(prob, saveat = tsave)
 data = [x => sol_data[x], z => sol_data[z]]
 p_prior = [σ => Normal(26.8, 0.1), β => Normal(2.7, 0.1)]
-p_posterior = @time bayesian_datafit(prob, p_prior, tsave, data)
+p_posterior = @time bayesian_datafit(prob, p_prior, tsave, data, niter = 3000)
 @test var.(getfield.(p_prior, :second)) >= var.(getfield.(p_posterior, :second))
 
 tsave1 = collect(10.0:10.0:100.0)
@@ -95,5 +95,5 @@ tsave2 = collect(10.0:13.5:100.0)
 sol_data2 = solve(prob, saveat = tsave2)
 data_with_t = [x => (tsave1, sol_data1[x]), z => (tsave2, sol_data2[z])]
 
-p_posterior = @time bayesian_datafit(prob, p_prior, data_with_t, niter = 200)
+p_posterior = @time bayesian_datafit(prob, p_prior, data_with_t, niter = 3000)
 @test var.(getfield.(p_prior, :second)) >= var.(getfield.(p_posterior, :second))
